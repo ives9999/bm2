6e5d10372396f98f357179e57a5fb104582456c1
ives <ives@housetube.tw>
2023-09-30 14:40:48 +0800

    1. add Error 2. member register continue.

--- src/Errors.js
+++ src/Errors.js
@@ -1,6 +1,6 @@
 
 const EMAILINVALID = 1000      // email不存在
-const PASSWORDINVALID = 1001   // password不符合
+const PASSWORDINVALID = 1001   // 密碼不符合
 const MEMBERINVALID = 1002     // 會員不存在
 const EMAILEXIST = 1003        // email已經註冊
 const EMAILBLANK = 1004        // email不能為空白
@@ -8,11 +8,16 @@ const PASSWORDBLANK = 1005     // 密碼不能為空白
 const NICKNAMEBLANK = 1006     // 暱稱不能為空白
 const NICKNAMEINVALID = 1007   // 暱稱不存在
 const NICKNAMEEXIST = 1008     // 暱稱已存在
+const REPASSWORDBLANK = 1009   // 確認密碼不能為空白
+const PASSWORDNOTMATCH = 1010  // 密碼不相符
+const NAMEBLANK = 1011         // 姓名不能為空白
+const NAMEINVALID = 1012       // 姓名不存在
+const NAMEEXIST = 1013         // 姓名已存在
 
 export function GetEmailBlankError() {
 
     const id = EMAILBLANK
-    const msg = "email不能為空值"
+    const msg = "email不能為空白"
     return {"id": id, "msg": msg}
 }
 
@@ -23,9 +28,86 @@ export function GetEmailInvalidError({email}) {
     return {"id": id, "msg": msg} 
 }
 
+export function GetMailExistError({email}) {
+
+    const id = EMAILEXIST
+    const msg = email + " email不存在"
+    return {"id": id, "msg": msg} 
+}
+
+export function GetPasswordInvalidError() {
+
+    const id = PASSWORDINVALID
+    const msg = "密碼不符合"
+    return {"id": id, "msg": msg}
+}
+
+export function GetPasswordBlankError() {
+
+    const id = PASSWORDBLANK
+    const msg = "密碼不能為空白"
+    return {"id": id, "msg": msg}
+}
+
+export function GetRePasswordBlankError() {
+
+    const id = REPASSWORDBLANK
+    const msg = "確認密碼不能為空白"
+    return {"id": id, "msg": msg}
+}
+
+export function GetPasswordNotMatchError() {
+
+    const id = PASSWORDNOTMATCH
+    const msg = "密碼不相符"
+    return {"id": id, "msg": msg}
+}
+
 export function GetNicknameBlankError() {
 
     const id = NICKNAMEBLANK
-    const msg = "暱稱不能為空值"
+    const msg = "暱稱不能為空白"
+    return {"id": id, "msg": msg}
+}
+
+export function GetNicknameInvalidError() {
+
+    const id = NICKNAMEINVALID
+    const msg = "暱稱不存在"
+    return {"id": id, "msg": msg}
+}
+
+export function GetNicknameExistError() {
+
+    const id = NICKNAMEEXIST
+    const msg = "暱稱已存在"
+    return {"id": id, "msg": msg}
+}
+
+export function GetMemberInvalidError() {
+
+    const id = MEMBERINVALID
+    const msg = "會員不存在"
+    return {"id": id, "msg": msg}
+}
+
+export function GetNameBlankError() {
+
+    const id = NAMEBLANK
+    const msg = "姓名不能為空白"
+    return {"id": id, "msg": msg}
+}
+
+export function GetNameInvalidError() {
+
+    const id = NAMEINVALID
+    const msg = "姓名不存在"
+    return {"id": id, "msg": msg}
+}
+
+export function GetNameExistError() {
+
+    const id = NAMEEXIST
+    const msg = "姓名已存在"
     return {"id": id, "msg": msg}
 }
--- src/pages/member/Register.js
+++ src/pages/member/Register.js
@@ -4,9 +4,68 @@ import Layout from '../../layout/Layout';
 import Input from "../../component/form/Input";
 import Password from "../../component/form/Password";
 
-import { GetEmailBlankError } from "../../Errors"
+import { 
+    GetEmailBlankError, 
+    GetNameBlankError, 
+    GetNicknameBlankError,
+    GetPasswordBlankError, 
+    GetRePasswordBlankError,
+    GetPasswordNotMatchError,
+ } from "../../Errors"
 
 const Register = () => {
+
+    //設定暱稱與初值
+    const [nickname, setNickname] = useState('')
+
+    //當暱稱值改變時，偵測最新的值
+    const handleNickname = (event) => {
+
+        var value = event.target.value
+        if (value.length > 0) {
+            setIsNicknameEmpty(false)
+            setNicknameErrorMsg("")
+        }
+        setNickname(value)
+    }
+
+    //偵測暱稱是否為空直，顯示錯誤訊息時使用
+    const [isNicknameEmpty, setIsNicknameEmpty] = useState(false)
+
+    //當按下清除暱稱文字按鈕後，清除暱稱
+    const handleClearNickname = (event) => {
+        event.preventDefault()
+        setNickname("")
+    }
+
+    //設定錯誤訊息
+    const [nicknameErrorMsg, setNicknameErrorMsg] = useState("")
+
+    //設定姓名與初值
+    const [myName, setMyName] = useState('')
+
+    //當姓名值改變時，偵測最新的值
+    const handleMyName = (event) => {
+
+        var value = event.target.value
+        if (value.length > 0) {
+            setIsMyNameEmpty(false)
+            setNameErrorMsg("")
+        }
+        setMyName(value)
+    }
+
+    //偵測姓名是否為空直，顯示錯誤訊息時使用
+    const [isMyNameEmpty, setIsMyNameEmpty] = useState(false)
+
+    //當按下清除姓名文字按鈕後，清除姓名
+    const handleClearMyName = (event) => {
+        event.preventDefault()
+        setMyName("")
+    }
+
+    //設定錯誤訊息
+    const [nameErrorMsg, setNameErrorMsg] = useState("")
 
     //設定email與初值
     const [email, setEmail] = useState('')
@@ -17,7 +76,7 @@ const Register = () => {
         var value = event.target.value
         if (value.length > 0) {
             setIsEmailEmpty(false)
-            setErrorMsg("")
+            setEmailErrorMsg("")
         }
         setEmail(value)
     }
@@ -31,17 +90,15 @@ const Register = () => {
         setEmail("")
     }
 
+    //設定錯誤訊息
+    const [emailErrorMsg, setEmailErrorMsg] = useState("")
+
 
     //設定密碼與初值
     const [password, setPassword] = useState('')
 
     //偵測密碼是否為空直，顯示錯誤訊息時使用
-    const [isPasswordEmpty, setIsPasswordEmpty] = useState(false)
-
-    //設定錯誤訊息
-    const [errorMsg, setErrorMsg] = useState("")
-
-    
+    const [isPasswordEmpty, setIsPasswordEmpty] = useState(false)    
 
     //當密碼值改變時，偵測最新的值
     const handlePassword = (event) => {
@@ -57,6 +114,32 @@ const Register = () => {
         event.preventDefault()
         setPassword("")
     }
+    //設定錯誤訊息
+    const [passwordErrorMsg, setPasswordErrorMsg] = useState("")
+
+    //設定密碼與初值
+    const [rePassword, setRePassword] = useState('')
+
+    //偵測密碼是否為空直，顯示錯誤訊息時使用
+    const [isRePasswordEmpty, setIsRePasswordEmpty] = useState(false)    
+
+    //當密碼值改變時，偵測最新的值
+    const handleRePassword = (event) => {
+        var value = event.target.value
+        if (value.length > 0) {
+            setIsRePasswordEmpty(false)
+        }
+        setRePassword(event.target.value)
+    }
+
+    //當按下清除密碼文字按鈕後，清除密碼
+    const handleClearRePassword = (event) => {
+        event.preventDefault()
+        setRePassword("")
+    }
+
+    //設定錯誤訊息
+    const [rePasswordErrorMsg, setRePasswordErrorMsg] = useState("")
 
     //按下送出後的動作
     const handleSubmit = (event) => {
@@ -66,13 +149,59 @@ const Register = () => {
         var isPass = false
         var params = {}
 
+        if (myName.length > 0) {
+            params["name"] = myName
+            isPass = true;
+        } else {
+            setIsMyNameEmpty(true)
+            setNameErrorMsg(GetNameBlankError().msg)
+            isPass = false
+        }
+
+        if (nickname.length > 0) {
+            params["nickname"] = nickname
+            isPass = true;
+        } else {
+            setIsNicknameEmpty(true)
+            setNicknameErrorMsg(GetNicknameBlankError().msg)
+            isPass = false
+        }
+
         if (email.length > 0) {
             params["email"] = email
             isPass = true;
         } else {
             setIsEmailEmpty(true)
-            setErrorMsg(GetEmailBlankError().msg)
+            setEmailErrorMsg(GetEmailBlankError().msg)
             isPass = false
+        }
+
+        if (password.length > 0) {
+            params["password"] = password
+            isPass = true;
+        } else {
+            setIsPasswordEmpty(true)
+            setPasswordErrorMsg(GetPasswordBlankError().msg)
+            isPass = false
+        }
+
+        if (rePassword.length > 0) {
+            params["repassword"] = rePassword
+            isPass = true;
+        } else {
+            setIsRePasswordEmpty(true)
+            setRePasswordErrorMsg(GetRePasswordBlankError().msg)
+            isPass = false
+        }
+
+        if (password !== rePassword) {
+            setIsRePasswordEmpty(true)
+            setRePasswordErrorMsg(GetPasswordNotMatchError().msg)
+            isPass = false
+        }
+
+        if (isPass) {
+        
         }
     }
 
@@ -89,14 +218,14 @@ const Register = () => {
                         label="暱稱"
                         type="text"
                         name="nickname"
-                        value={email}
+                        value={nickname}
                         id="nickname"
                         placeholder="羽神"
                         isRequired={true}
-                        isError={isEamilEmpty}
-                        errorMsg={errorMsg}
-                        onChange={handleEmail}
-                        onClear={handleClearEmail}
+                        isError={isNicknameEmpty}
+                        errorMsg={nicknameErrorMsg}
+                        onChange={handleNickname}
+                        onClear={handleClearNickname}
                     />
                     <Input 
                         label="Email"
@@ -107,7 +236,7 @@ const Register = () => {
                         placeholder="you@example.com"
                         isRequired={true}
                         isError={isEamilEmpty}
-                        errorMsg={errorMsg}
+                        errorMsg={emailErrorMsg}
                         onChange={handleEmail}
                         onClear={handleClearEmail}
                     />
@@ -119,22 +248,46 @@ const Register = () => {
                         placeholder="請填密碼"
                         isRequired={true}
                         isError={isPasswordEmpty}
-                        errorMsg={errorMsg}
+                        errorMsg={passwordErrorMsg}
                         onChange={handlePassword}
                         onClear={handleClearPassword}
                     />
-                    
-                    <a href="/member/forget_password" className="text-textTitleColor text-sm">忘記密碼？</a>
 
+                    <Password 
+                        label="確認密碼"
+                        name="repassword"
+                        value={rePassword}
+                        id="repassword"
+                        placeholder="請填確認密碼"
+                        isRequired={true}
+                        isError={isRePasswordEmpty}
+                        errorMsg={rePasswordErrorMsg}
+                        onChange={handleRePassword}
+                        onClear={handleClearRePassword}
+                    />
+                    <Input 
+                        label="姓名"
+                        type="text"
+                        name="name"
+                        value={myName}
+                        id="name"
+                        placeholder="王小明"
+                        isRequired={true}
+                        isError={isMyNameEmpty}
+                        errorMsg={nameErrorMsg}
+                        onChange={handleMyName}
+                        onClear={handleClearMyName}
+                    />
+                    
                     <button
                         type="button"
                         className="rounded-md w-full h-12 mt-8 bg-myPrimary px-5 py-1 text-sm font-semibold text-myBlack shadow-sm hover:text-myWhite focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         onClick={handleSubmit}
                     >
-                        登入
+                        註冊
                     </button>
 
-                    <div className="text-menuTextWhite text-sm mt-3">還沒有帳號，請<a className="text-myPrimary text-sm" href="/member/register">註冊</a></div>
+                    <div className="text-menuTextWhite text-sm mt-3">已經有帳號，請<a className="text-myPrimary text-sm" href="/member/login">登入</a></div>
                 </div>
             </form>  
         </div>    
