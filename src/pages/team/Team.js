a0050acfd8a70562e7641707780299d8c3e50ef6
ives <ives@housetube.tw>
2023-10-30 11:08:35 +0800

    球隊首頁開始

--- src/pages/team/Team.js
+++ src/pages/team/Team.js
@@ -1,10 +1,22 @@
 import Layout from '../../layout/Layout';
+import Breadcrumb from '../../layout/Breadcrumb'
+
+const breadcrumbs = [
+    { name: '球隊', href: '/team', current: true },
+]
 
 const Team = () => {
     return (
         <>
         <Layout>
-            aaa
+        <div className="mx-auto max-w-7xl">
+            <main className="isolate">
+                <Breadcrumb items={breadcrumbs}/>
+                <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-1 lg:grid-cols-2 xl:gap-x-8">
+
+                </div>
+            </main>
+        </div>
         </Layout>
         </>
     );
