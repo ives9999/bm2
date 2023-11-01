fd16250e8abfdad1598291dd7de752f206276b6f
ives <ives@housetube.tw>
2023-11-01 10:07:12 +0800

    球隊首頁，由取得api取得資料完成

--- src/pages/team/Team.js
+++ src/pages/team/Team.js
@@ -1,11 +1,36 @@
+import { React, useState, useEffect } from "react";
+import axios from "axios";
 import Layout from '../../layout/Layout';
 import Breadcrumb from '../../layout/Breadcrumb'
+import { dump } from "../../functions"
+
+const api = process.env.REACT_APP_API + "/team"
+const domain = process.env.REACT_APP_ASSETS_DOMAIN
 
 const breadcrumbs = [
     { name: '球隊', href: '/team', current: true },
 ]
 
+
 const Team = () => {
+
+    const [ team, setTeam ] = useState([])
+    useEffect(() => {
+        // const config = {
+        //     method: "GET",
+        //     Headers: {
+        //         'Content-Type': 'application/json'
+        //     }
+        // }
+        axios.get(api)
+        .then(response => {
+            dump(response)
+            //if (response.data.success) {
+                setTeam(response.data)
+            //}
+        })
+    },[])
+
     return (
         <>
         <Layout>
