24ff2d1cf085b5f4c9faf46b7e05d3427a42d2db
ives <ives@housetube.tw>
2023-11-01 21:01:14 +0800

    球隊首頁完成

--- src/pages/team/Team.js
+++ src/pages/team/Team.js
@@ -2,6 +2,8 @@ import { React, useState, useEffect } from "react";
 import axios from "axios";
 import Layout from '../../layout/Layout';
 import Breadcrumb from '../../layout/Breadcrumb'
+import { UserIcon } from '@heroicons/react/24/outline'
+
 import { dump } from "../../functions"
 
 const api = process.env.REACT_APP_API + "/team"
@@ -14,7 +16,10 @@ const breadcrumbs = [
 
 const Team = () => {
 
-    const [ team, setTeam ] = useState([])
+    const [ team, setTeam ] = useState({rows: []})
+    const toTeam = (id) => {
+        window.location.href = "/team/" + id
+    }
     useEffect(() => {
         axios.get(api)
         .then(response => {
@@ -31,8 +36,8 @@ const Team = () => {
                 <Breadcrumb items={breadcrumbs}/>
                 <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-1 lg:grid-cols-2 xl:gap-x-8">
                     {team.rows.map((row) => (
-                        <div className="bg-blockColor rounded-md border border-borderColor">
-                            <div key={row.id} className="group relative">
+                        <div key={row.id} className="bg-blockColor rounded-md border border-borderColor">
+                            <div className="group relative">
                                 <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-80">
                                     <a href={"/team/" + row.id}>
                                     <img
@@ -41,6 +46,37 @@ const Team = () => {
                                         className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                                     />
                                     </a>
+                                </div>
+                            </div>
+                            <div className="mt-4 justify-between">
+                                <div className="mb-6 flex flex-row justify-between">
+                                    <div className=""><a className="text-tagColor text-sm hover:text-focusBlue" href={"/arena/" + row.arena.id}>{row.arena.name}</a></div>
+                                    <div className="">
+                                        <div className="text-tagColor text-sm hover:text-focusBlue flex">
+                                            <UserIcon className="h-5 w-5 align-middle" aria-hidden="true" />
+                                            <div className="">{row.pv} 次</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href={"/team/" + row.id} className="text-textTitleColor text-textTitleSize hover:text-focusBlue focus:text-focusBlue">{row.name}</a>
+                                <div className="mt-8 mb-6 flex flex-row justify-between">
+                                    <div className="text-base text-tagColor hover:text-focusBlue focus:text-focusBlue flex flex-row">
+                                        <a className="" href={"/member/" + row.member["id"]}><img className="w-12 h-12 rounded-full" src={domain + row.member["avatar"]} alt={row.member["nickname"]} /></a>
+                                        <div className="-mt-2">
+                                            <a href={"/member/" + row.member["token"]} className="text-base text-tagColor hover:text-focusBlue focus:text-focusBlue ms-2">{row.member["nickname"]}</a>
+                                            <div className="text-base text-tagColor hover:text-focusBlue focus:text-focusBlue ms-2">{row.member["created_at"]}</div>
+                                        </div>
+                                    </div>
+                                    <button
+                                        type="button"
+                                        className="rounded-md bg-background px-5 py-1 text-sm font-semibold text-primaryText shadow-sm hover:text-myPrimary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                        onClick={(e) => {
+                                            e.preventDefault()
+                                            toTeam(row.id)
+                                        }}
+                                    >
+                                        更多...
+                                    </button>
                                 </div>
                             </div>
                         </div>
