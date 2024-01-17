7c541a9f0595280ceb0f4d7638b01828608d3649
ives <ives@housetube.tw>
2024-01-17 09:50:42 +0800

    後臺開始

--- src/pages/admin/Index.js
+++ src/pages/admin/Index.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Avatar, Dropdown, Navbar } from 'flowbite-react';
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 
 function Index() {
     return (
-        <div>
-            Admin
-        </div>
+        <>
+        <nav className="flex px-2 py-2.5 border-gray-700 bg-BG-900 sm:px-4 rounded">
+            <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+        </nav>
+        </>
     )
 }
 
