5d5f6eb7d3634acb3f20b3b49ceb9d09a76445cb
ives <ives@bluemobile.com.tw>
2024-02-23 14:17:43 +0800

    header update again start.

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -3,6 +3,7 @@ import { useNavigate, Link } from 'react-router-dom';
 import BMContext from '../context/BMContext'
 import {PrimaryButton} from '../component/MyButton'
 import Logo from '../component/Logo'
+import Example from "../component/Dropdown";
 
 const Header = () => {
     const {auth, setAuth} = useContext(BMContext)
@@ -38,22 +39,63 @@ const Header = () => {
         memberItems.unshift({name: '後台', href: '/admin',})
     }
 
+
     return (
         <header>
             <nav className="px-4 lg:px-6 py-4">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Logo url="/" />
-                    <div className={`
-                        ${!isLogin ? "lg:order-3" : "hidden"}
-                    `}>
-                        <PrimaryButton onClick={()=>navigate("/member/login")}>登入</PrimaryButton>
+                    <div className="hidden justify-between items-center w-full lg:flex lg:w-auto">
+                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                            {items.map(item => (
+                                <li key={item.name}>
+                                    <Link to={item.href} className={`
+                                        block px-10 py-1 text-xm
+                                        ${item.current ? "rounded-full bg-Primary-300 text-MyBlack" : "text-MyWhite hover:rounded-full hover:bg-Primary-300 hover:text-MyBlack"}
+                                    `}>
+                                        {item.name}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    
+                    <div className='pr-4'>
+                        {!isLogin ?
+                            <div>
+                                <PrimaryButton onClick={()=>navigate("/member/login")}>登入</PrimaryButton>
+                            </div>
+                        :
+                            <div className="flex gap-4 items-center">
+                                <div>
+                                    <Example />
+                                    {/* <Menu>
+                                        <Menu.Button>
+                                            <img src={auth.avatar} className="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500 cursor-pointer" alt={auth.nickname} />
+                                        </Menu.Button>
+                                        <Menu.Items>
+                                        {memberItems.map(memberItem => (
+                                            <Menu.Item 
+                                                key={memberItem.href} 
+                                                as='a'
+                                                href={memberItem.href}
+                                                className="bg-blue-500 text-white"
+                                            >
+                                                    {memberItem.name}
+                                            </Menu.Item>
+                                        ))}
+                                        </Menu.Items>
+                                    </Menu> */}
+                                </div>
+                                <span className="h-6 flex items-center bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-Primary-400 border border-Primary-400">{auth.nickname}</span>
+                            </div>
+                        }
                     </div>
 
-                    <div className={` 
+                    {/* <div className={` 
                         ${isLogin ? "flex items-center lg:order-2" : "hidden"}
                     `}>
                         <button type="button" className="flex mx-3 text-sm md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="dropdown">
-                            {/* <span className="sr-only">Open user menu</span> */}
                             <img className="w-10 h-9 rounded-full p-1 ring-2 ring-gray-300 dark:ring-gray-500" src={auth.avatar} alt={auth.nickname} />
                         </button>
                         <div className="hidden z-50 my-4 w-56 text-base list-none bg-PrimaryBlock-950 border border-PrimaryBlock-800 rounded-lg divide-y divide-MenuDivider shadow" id="dropdown">
@@ -80,22 +122,9 @@ const Header = () => {
                             <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                             <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                         </button>
-                    </div>
+                    </div> */}
 
-                    <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                            {items.map(item => (
-                                <li key={item.name}>
-                                    <Link to={item.href} className={`
-                                        block px-10 py-1 text-xm
-                                        ${item.current ? "rounded-full bg-Primary-300 text-MyBlack" : "text-MyWhite hover:rounded-full hover:bg-Primary-300 hover:text-MyBlack"}
-                                    `}>
-                                        {item.name}
-                                    </Link>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
+                    
                 </div>
             </nav>
         </header>
