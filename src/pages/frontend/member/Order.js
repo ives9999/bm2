d5367227d57b60a74a7d3176073d8640a62a689f
ives <ives@housetube.tw>
2024-06-28 10:59:53 +0800

    前端會員檢視訂單完成

--- src/pages/frontend/member/Order.js
+++ src/pages/frontend/member/Order.js
@@ -7,6 +7,7 @@ import useQueryParams from '../../../hooks/useQueryParams'
 import {Pagination} from '../../../component/Pagination'
 import { formattedWithSeparator } from '../../../functions/math'
 import { noSec } from '../../../functions/date'
+import Divider from '../../../component/Divider'
 
 export default function Order() {
     const {auth, setIsLoading, setAlertModal, isLoading} = useContext(BMContext);
@@ -76,50 +77,55 @@ export default function Order() {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [_page, isLoading])
 
+    if (isLoading) { return <div className="text-MyWhite">loading</div>}
+    else {
     return (
         <div>
             <div className="mx-auto max-w-7xl">
                 <main className="isolate px-1">
                     <Breadcrumb items={breadcrumbs}/>
                     <h2 className="text-Primary-300 text-center text-4xl font-bold mb-10">訂單</h2>
-                    <div className="w-full bg-Menu border border-PrimaryBlock-800 p-6 rounded-lg">
+                    <div className="w-full bg-Menu border border-PrimaryBlock-800 px-2 lg:p-6 rounded-lg">
                         {rows.map(row =>
-                            <div key={row.order_no} className='my-4 p-2 text-white bg-blockColor'>
-                                <div className='flex flex-row items-center justify-between'>
-                                    <div className='flex flex-row items-center'>
-                                        <div>{row.order_no}</div>
-                                        <div className='flex flex-col gap-2 ml-4'>
-                                            <div className='flex flex-row items-center gap-2'><span className='text-xs'>NT$</span> <span className='text-xl text-Warning-400'>{formattedWithSeparator(row.grand_total)}</span></div>
+                            <div key={row.order_no} className='text-white'>
+                                <div className='flex flex-row items-center justify-between bg-blockColor my-4 lg:p-4 lg:pl-4 text-MyWhite'>
+                                    <div className='lg:mr-4 flex flex-row gap-4'>
+                                        <div className=''>{noSec(row.created_at, true)}</div>
+                                        <div className='flex flex-row items-center'>
+                                            <span className='text-xs mr-2'>總額：NT$</span> 
+                                            <span className='text-xl text-Warning-400'>{formattedWithSeparator(row.grand_total)}</span>
                                         </div>
                                     </div>
-                                    <div className='text-white ml-4'>{noSec(row.created_at, true)}</div>
+
+                                    <div className='flex flex-row items-center'>{row.order_no}</div>
                                 </div>
                                 <div className='mt-4'>
                                 {row.items.map(item =>
-                                    <div key={item.id} className='text-white mb-8'> 
-                                        <div className='flex flex-row justify-between'>
-                                            <div className="flex flex-row items-center gap-4">
-                                                <div className="w-[50px] xl:w-[200px]">
-                                                    {item.product.images && item.product.images.length > 0
-                                                        ? <img src={item.product.images[0].path} alt={item.product.name} />
-                                                        : ''
-                                                    }
-                                                </div>
-                                                <div className=''>{item.product.name}</div>
+                                    <div key={item.id} className='py-4 lg:p-4 text-white bg-blockColor'> 
+                                        <div className='flex flex-row items-center gap-2 mb-4 pb-4'>
+                                            <div className="w-[100px] xl:w-[200px] lg:mr-4">
+                                                {item.product.images && item.product.images.length > 0
+                                                    ? <img src={item.product.images[0].path} alt={item.product.name} />
+                                                    : ''
+                                                }
                                             </div>
-                                            <div className="flex flex-row items-center gap-4">
-                                                <div className=''>{formattedWithSeparator(item.sell_price)}</div>
-                                                <div className=''>x{item.quantity}</div>
-                                                <div className=''>{formattedWithSeparator(item.total_amount)}</div>
+                                            <div className="flex-grow">
+                                                <div className='mb-2'>{item.product.name}</div>
+                                                <div className='flex flex-row items-center justify-between'>
+                                                    <div className=''>單價：$&nbsp;<span className='text-Warning-400'>{formattedWithSeparator(item.sell_price)}</span></div>
+                                                    <div className='lg:mr-6'>x{item.quantity}</div>
+                                                </div>
                                             </div>
                                         </div>
+                                        <div className='flex flex-row justify-end items-center mt-2'>
+                                            <div className='flex items-center'>
+                                                <span className='text-xs mr-2'>總額：NT$</span> 
+                                                <span className='text-xl text-Warning-400'>{formattedWithSeparator(item.total_amount)}</span>
+                                            </div>
+                                        </div>
+                                        <Divider text='分隔線' textColor='PrimaryBlock-400' textSize='lg:text-base text-xs' />
                                     </div>
                                 )}
-                                </div>
-                                <hr />
-                                <div className='flex flex-row justify-end items-center mt-2'>
-                                    <span className='text-xs mr-2'>NT$</span> 
-                                    <span className='text-xl text-Warning-400'>{formattedWithSeparator(row.grand_total)}</span>
                                 </div>
                             </div>
                         )}
@@ -127,5 +133,5 @@ export default function Order() {
                 </main>
             </div>
         </div>
-    )
+    )}
 }
